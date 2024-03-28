99be14cf2b0696bef43a797f61aa15c11312efbc
Kirik-hamster <mkir2@k.ru>
2024-03-28 10:07:37 +0400

    Initial commit

---
+++ grabber.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	src := flag.String("src", "", "Source file path")
+	dst := flag.String("dst", "", "Destination directory path")
+	flag.Parse()
+
+	if *src == "" || *dst == "" {
+		log.Fatal("Source file path and destination directory path must be specified. Use --src and --dst flags to specify them.")
+	}
+
+	srcFile, err := os.Open(*src)
+	if err != nil {
+		log.Fatalf("Error opening source file: %v\n", err)
+	}
+	defer srcFile.Close()
+
+	var wg sync.WaitGroup
+	scanner := bufio.NewScanner(srcFile)
+
+	for scanner.Scan() {
+		urlStr := strings.TrimSpace(scanner.Text())
+		if urlStr == "" {
+			continue
+		}
+
+		wg.Add(1)
+		go func(urlStr string) {
+			defer wg.Done()
+			fetchAndSave(urlStr, *dst)
+		}(urlStr)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading source file: %v\n", err)
+	}
+
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Printf("\nProgram execution time: %s\n", elapsed)
+}
+
+func fetchAndSave(urlStr, dst string) {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		log.Printf("Error fetching URL %s: %v\n", urlStr, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Unexpected status code for URL %s: %d\n", urlStr, resp.StatusCode)
+		return
+	}
+
+	fileName, err := getFileNameFromURL(urlStr)
+	if err != nil {
+		log.Printf("Error getting file name from URL %s: %v\n", urlStr, err)
+		return
+	}
+	fileName += ".html"
+	filePath := filepath.Join(dst, fileName)
+
+	dstFile, err := os.Create(filePath)
+	if err != nil {
+		log.Printf("Error creating destination file %s: %v\n", filePath, err)
+		return
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, resp.Body)
+	if err != nil {
+		log.Printf("Error copying content to file %s: %v\n", filePath, err)
+		return
+	}
+
+	fmt.Printf("File copied successfully to %s\n", filePath)
+}
+
+func getFileNameFromURL(siteURL string) (string, error) {
+	parsedURL, err := url.Parse(siteURL)
+	if err != nil {
+		return "", err
+	}
+
+	domain := strings.TrimPrefix(parsedURL.Host, "www.")
+	parts := strings.SplitN(domain, ".", 2)
+	if len(parts) < 2 {
+		return domain, nil
+	}
+
+	return parts[0], nil
+}
